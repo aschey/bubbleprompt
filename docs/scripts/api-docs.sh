68ab9e00e85be75ecf8130b56c23b5bbe4724d98
Austin Schey <aschey13@gmail.com>
2022-11-27 18:46:55 -0600

    change relative paths

--- docs/scripts/api-docs.sh
+++ docs/scripts/api-docs.sh
@@ -1,5 +1,5 @@
 (cd "$(dirname $0)/../.." &&
-    godocdown -template=./docs/scripts/md.tmpl .\\input\\simpleinput > ./docs/docs/03-Input/02-Simple/API.mdx &&
-    godocdown -template=./docs/scripts/md.tmpl .\\input\\commandinput > ./docs/docs/03-Input/03-Command/API.mdx &&
-    godocdown -template=./docs/scripts/md.tmpl .\\input\\lexerinput > ./docs/docs/03-Input/04-Lexer/API.mdx &&
-godocdown -template=./docs/scripts/md.tmpl .\\input\\parserinput > ./docs/docs/03-Input/05-Parser/API.mdx)
+    godocdown -template=./docs/scripts/md.tmpl ./input/simpleinput > ./docs/docs/03-Input/02-Simple/API.mdx &&
+    godocdown -template=./docs/scripts/md.tmpl ./input/commandinput > ./docs/docs/03-Input/03-Command/API.mdx &&
+    godocdown -template=./docs/scripts/md.tmpl ./input/lexerinput > ./docs/docs/03-Input/04-Lexer/API.mdx &&
+godocdown -template=./docs/scripts/md.tmpl ./input/parserinput > ./docs/docs/03-Input/05-Parser/API.mdx)
